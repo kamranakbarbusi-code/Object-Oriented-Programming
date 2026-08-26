ed09ca07d9277a985bb2a52233aeb0833d5967f8
kamranakbarbusi-code <kamranakbarbusi@gmail.com>
2026-08-26 09:45:21 -0500

    Add files via upload

---
+++ PP1.py
@@ -0,0 +1,10 @@
+print("Enter Employee's Name:")
+emp_name = input()
+
+print("Enter basic pay:")
+basic_pay = int(input())
+
+print("Deduction amount:")
+deduction_1 = int(input())
+
+print(emp_name+"'s","total pay is", basic_pay - deduction_1)
---
+++ main.py
@@ -0,0 +1,10 @@
+print("Enter Value of a:")
+a = int(input())
+
+print("Enter Value of b:")
+b = int(input())
+
+c = a + b
+
+print("The summation of a and b=:", c)
+
